8cbc26ef2da6abd76437e743c52b35aa578e9579
gihwanJang <90659997+gihwanJang@users.noreply.github.com>
2024-07-04 15:33:17 +0900

    test: add test

--- src/test/java/com/nhnacademy/client/controller/ClientControllerTest.java
+++ src/test/java/com/nhnacademy/client/controller/ClientControllerTest.java
@@ -11,7 +11,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
--- src/test/java/com/nhnacademy/client/dto/request/ClientChangePasswordRequestDtoTest.java
+++ src/test/java/com/nhnacademy/client/dto/request/ClientChangePasswordRequestDtoTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
-public class ClientChangePasswordRequestDtoTest {
+class ClientChangePasswordRequestDtoTest {
 
     private Validator validator;
 
